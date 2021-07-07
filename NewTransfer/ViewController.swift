e4296e32cc42d74336fb6b57f5a180f27655d5b2
Jongho Lee <bazinga940@gmail.com>
2021-07-07 09:37:35 +0900

    project 생성

---
+++ NewTransfer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTransfer
+//
+//  Created by Jongho Lee on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
