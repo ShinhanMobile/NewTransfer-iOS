a741a4e55d1e9780bd5d9315a57ae06b703a4c0f
Jongho Lee <bazinga940@gmail.com>
2021-07-08 11:04:04 +0900

    파일 미리 생성

---
+++ NewTransfer/NetworkManager/ApiConfig.swift
@@ -0,0 +1,8 @@
+//
+//  ApiConfig.swift
+//  NewTransfer
+//
+//  Created by Jongho Lee on 2021/07/08.
+//
+
+import Foundation
---
+++ NewTransfer/NetworkManager/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  NewTransfer
+//
+//  Created by Jongho Lee on 2021/07/08.
+//
+
+import Foundation
